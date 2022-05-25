6d5e0e9e00e8c2005c07ed6b6058e2de49e6e57c
assisMeneghetti <assismfabri@hotmail.com>
2022-05-25 13:59:46 -0300

    feat: filter all created_by values >+ 3 from purchase_orders table desc order

---
+++ desafio10.sql
@@ -0,0 +1,22 @@
+SELECT 
+    id,
+    supplier_id,
+    created_by,
+    submitted_date,
+    creation_date,
+    status_id,
+    expected_date,
+    shipping_fee,
+    taxes,
+    payment_date,
+    payment_amount,
+    payment_method,
+    notes,
+    approved_by,
+    approved_date,
+    submitted_by
+FROM
+    purchase_orders
+WHERE
+    created_by >= 3
+ORDER BY created_by DESC , id;
